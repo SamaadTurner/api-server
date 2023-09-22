8967a4d06c38bf634afb2e3c5aaad87401522e32
SamaadTurner <samaad.turner@gmail.com>
2023-09-21 23:50:33 -0700

    good start on lab

--- src/models/coach.js
+++ src/models/coach.js
@@ -1,14 +1,18 @@
 'use strict';
 
-const CoachModel = (sequelize, DataTypes) => sequelize.define('Coach', {
-    name: { 
+const CoachModel = (sequelize, DataTypes) => sequelize.define('Coach', {// define a new table called Coach
+    name: { // define a new column called name 
         type: DataTypes.STRING,
          allowNull: false 
         },
-    championships: {
+    championships: { // define a new column called championships
         type: DataTypes.INTEGER,
         allowNull: false
         },
+        playerId: { // foreign key
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
 });
 
 module.exports = CoachModel;
--- src/models/index.js
+++ src/models/index.js
@@ -1,15 +1,25 @@
 'use strict';
-
-const { Sequelize, DataTypes } = require('sequelize');
-const Player = require('./player.js');
-const Coach = require('./coach.js');
-
+require('dotenv').config(); 
 const SQL_CONNECTION_STRING = process.env.SQL_CONNECTION_STRING || 'sqlite:memory:';
+const { Sequelize, DataTypes } = require('sequelize');
+const players = require('./player.js');
+const coaches = require('./coach.js');
+const Collection = require('./collection-class.js');
+
 
 const sequelize = new Sequelize(SQL_CONNECTION_STRING); // this can only be done once (it is a singleton)
+
+const CoachModel = coaches(sequelize, DataTypes);
+const PlayerModel = players(sequelize, DataTypes);
+
+
+
 
 module.exports = {
     sequelize,
     PlayerModel: Player(sequelize, DataTypes),
     CoachModel: Coach(sequelize, DataTypes)   
 };
+
+
+// NOTE: coaches has players and players have one coach
